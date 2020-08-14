031fd28d5de761544d368d10ef8db0727e166181
Enes Terzi <38747547+enestrz@users.noreply.github.com>
2020-08-14 22:49:33 +0300

    Update connection.py

--- BankaVeritabanıApp/connection.py
+++ BankaVeritabanıApp/connection.py
@@ -1,3 +1,3 @@
 import psycopg2
 
-connection = psycopg2.connect("dbname=BankaVeritabani user=postgres password=12357896")
+connection = psycopg2.connect("dbname=BankaVeritabani user=postgres password=******")
